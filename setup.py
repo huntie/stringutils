410bb6824e5441030df79a1e403e249f1c15a430
Alex Hunt <hello@alexhunt.io>
2018-01-28 17:45:47 +0000

    Add README as long_description in setup.py, add keywords

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+from codecs import open
 from setuptools import setup
 from stringutils import __version__
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='stringutils',
     description='A functional string utility library for Python',
+    long_description=long_description,
     version=__version__,
     url='https://github.com/huntie/stringutils',
     author='Alex Hunt',
@@ -19,5 +24,6 @@ setup(
         'Topic :: Utilities',
     ],
 
-    packages=['stringutils'],
+    keywords='string helpers transform',
+    packages=['stringutils']
 )
